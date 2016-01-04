315e88249ceceb0e9d6db1f73319ca6500847357
Vladimir I. Puzakov <vppuzakov@gmail.com>
2016-01-05 02:26:18 +0300

    add models to training module

---
+++ Memoizator.Modules.Training/Models/Term.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memoizator.Modules.Training.Models
+{
+    public class Term
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public List<Topic> ContainingTopics { get; }
+
+        public Term(Guid id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ContainingTopics = new List<Topic>();
+        }
+    }
+}
---
+++ Memoizator.Modules.Training/Models/Topic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memoizator.Modules.Training.Models
+{
+    public class Topic
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public Topic Parent { get; }
+        public List<Term> Terms { get; } 
+
+        public Topic(Guid id, string name, string description, Topic parent)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Parent = parent;
+            Terms = new List<Term>();
+        }
+
+        public void Add(Term term)
+        {
+            Terms.Add(term);
+            term.ContainingTopics.Add(this);
+        }
+
+        public void Remove(Term term)
+        {
+            Terms.Remove(term);
+            term.ContainingTopics.Remove(this);
+        }
+    }
+}
