527542130385dfe65eed56458b124e2766c15623
Vladimir I. Puzakov <vppuzakov@gmail.com>
2016-01-04 21:15:07 +0300

    Add training module

---
+++ Memoizator.Modules.Training/TrainingModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Prism.Modularity;
+
+namespace Memoizator.Modules.Training
+{
+    public class TrainingModule : IModule
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
